bfabb8b472d21421fefccc332647e52b021c5302
Zamm <azzamymuttaqin2@gmail.com>
2025-10-31 17:58:26 +0800

    Add files via upload

---
+++ scrip.js
@@ -0,0 +1,17 @@
+let long = prompt("Masukkan p Persegi panjang")
+let wide = prompt("Masukkan l Persegi panjang")
+let area = long * wide
+alert("Luas persegi panjang = " + area + " cm2")
+var name = "Hafiz"
+name = "Azzam" // bisa diubah
+var name = "Sal" // bisa di deskripsikan ulang
+console.log(name)
+
+let address = "Jakarta"
+address = "Surabaya" // bisa diubah
+// let address = "Samarinda" // tidak bisa dideskripsikan ulang
+console.log(address)
+
+const pi = 3.14
+// pi = 2 // tidak bisa diubah dan deskripsikan ulang
+console.log(pi)
